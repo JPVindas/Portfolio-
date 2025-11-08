ee009364681250cf76523297e471f3700a1e7615
JPVindas <jvindas40886@ufide.ac.cr>
2025-11-07 18:07:40 -0600

    push

--- JPV-Portfolio/vite.config.ts
+++ JPV-Portfolio/vite.config.ts
@@ -5,6 +5,7 @@ import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio-/", // 👈 DEBE coincidir
   server: { port: 5173 },
   css: {
     postcss: {
